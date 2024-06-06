8bc4647584995c7a3cd152f0ad9afda7821ef27e
icelandld815 <icelandld@163.com>
2024-06-06 16:50:08 +0800

    Update cypress.config.ts

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   fixturesFolder: false,
+  projectId: "ohibgb",
   e2e: {
     setupNodeEvents(on, config) {},
     baseUrl: 'http://localhost:8888',
